f0d0258144148b2df72b90c8169a65859dda0796
foumzguiddiscoverycamp-sudo <foumzguiddiscoverycamp@gmail.com>
2025-12-26 14:11:50 +0100

    Add files via upload

---
+++ file.sh
@@ -0,0 +1,10 @@
+# إذا لم تكن مثبت Git
+# قم بتنزيله من git-scm.com
+
+# افتح Terminal/CMD في مجلد مشروعك
+git init
+git add .
+git commit -m "Initial commit - Foum Zguid Discovery Camp"
+git branch -M main
+git remote add origin https://github.com/username/foumzguid-discovery-camp.git
+git push -u origin main
